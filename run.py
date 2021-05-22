558723b0fd09eb3c5b8a8c503569c9beca77c437
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-05-21 23:17:45 -0300

    fixando dimensao datela

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from app.app import App
 
 root = Tk()
 root["bg"] = "#6DDE81"
-root.title("Verificador de planta")
-root.minsize(640, 400)
+root.title("Verificador de doença de planta")
+root.geometry('640x400')
+root.resizable(width=0, height=0)
 
 App(root)
 root.mainloop()
